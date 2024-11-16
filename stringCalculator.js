086b9e0ecc09738164f65505901c4edc438a4ec8
Jyotsana Khaparde <jyotsanakhaparde@Jyotsanas-MacBook-Air.local>
2024-11-16 19:14:22 +0530

    Handle One or Two Numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,5 +1,8 @@
 function add(numbers) {
-  return numbers ? 0 : 0;
+  if (!numbers) return 0;
+
+  const numArray = numbers.split(",").map(Number);
+  return numArray.reduce((sum, num) => sum + num, 0);
 }
 
-export default add;
+module.exports = { add };
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,5 +1,13 @@
-import add from "./stringCalculator";
+const { add } = require("./stringCalculator");
 
 test("returns 0 for an empty string", () => {
   expect(add("")).toBe(0);
+});
+
+test("returns the number itself if only one number is provided", () => {
+  expect(add("1")).toBe(1);
+});
+
+test("returns the sum of two comma-separated numbers", () => {
+  expect(add("1,5")).toBe(6);
 });
